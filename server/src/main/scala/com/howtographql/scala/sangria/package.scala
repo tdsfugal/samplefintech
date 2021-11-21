45dc70d038f6bc5fdb954e7a413146c85dc5739e
TDSFugal <tim.smith@fugaltech.com>
2021-11-20 21:17:10 -0500

    Update schema

--- server/src/main/scala/com/howtographql/scala/sangria/package.scala
+++ server/src/main/scala/com/howtographql/scala/sangria/package.scala
@@ -1,5 +1,5 @@
-package com.howtographql.scala.sangria
+package com.howtographql.scala.sangria.models_
 
 package object models {
-  
+  case class Link(id: Int, url: String, description: String)
 }
